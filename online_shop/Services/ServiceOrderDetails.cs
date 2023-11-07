c6252ea7c5dd74d9c119419333e2aae9a4d06322
Dancasiu Flavius <dancasiu.flavius@yahoo.com>
2023-11-08 00:29:00 +0200

    Customer Making

--- online_shop/Services/ServiceOrderDetails.cs
+++ online_shop/Services/ServiceOrderDetails.cs
@@ -58,10 +58,55 @@ namespace online_shop.Services
                 Console.WriteLine("An error occurred while reading the file: " + e.Message);
             }
         }
-        public void ShowCustomers()
+        public void ShowOrderDetails()
         {
             for (int i = 0; i < _ordersDetailsList.Count; i++)
                 Console.WriteLine(_ordersDetailsList[i].GetOrderDetails());
+        }
+        public bool FindOrderDetailsByID(OrderDetails order)
+        {
+            for (int i = 0; i < _ordersDetailsList.Count(); i++)
+            {
+                if (order.GetOrderID().Equals(_ordersDetailsList[i].GetOrderID()))
+                    return true;
+            }
+            return false;
+        }
+        public bool AddOrderDetails(OrderDetails orderDetails)
+        {
+            if (FindOrderDetailsByID(orderDetails) == true)
+                return false;
+            else
+                _ordersDetailsList.Add(orderDetails);
+            return true;
+        }
+        public bool RemoveOrderDetails(String id)
+        {
+            for (int i = 0; i < _ordersDetailsList.Count; i++)
+            {
+                if (_ordersDetailsList[i].GetOrderID().Equals(id))
+                {
+                    _ordersDetailsList.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool UpdateOrderDetails(String id, String order_Id, String product_id, int price, int qty, String newId)
+        {
+            for (int i = 0; i < _ordersDetailsList.Count; i++)
+            {
+                if (_ordersDetailsList[i].GetOrderID().Equals(id))
+                {
+                    _ordersDetailsList[i].SetOrderID(order_Id);
+                    _ordersDetailsList[i].SetProductID(product_id);
+                    _ordersDetailsList[i].SetPrice(price);
+                    _ordersDetailsList[i].SetPrice(qty);
+                    _ordersDetailsList[i].SetOrderID(newId);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
---
+++ online_shop/Views/ViewCustomer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_shop.Views
+{
+    internal class ViewCustomer
+    {
+    }
+}
